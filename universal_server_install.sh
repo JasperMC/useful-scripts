966f0c3b0f6dc6c723230a56487f5a8c318d57ea
Jasper C <37154053+JasperMC@users.noreply.github.com>
2021-11-20 21:07:28 +0100

    Update universal_server_install.sh

--- universal_server_install.sh
+++ universal_server_install.sh
@@ -18,7 +18,7 @@ sudo apt-get install \
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-echo \
+sudo echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
@@ -29,8 +29,8 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io -y
  
  
 # Install Portainer
-docker volume create portainer_data
-docker run -d -p 8000:8000 -p 9443:9443 --name portainer \
+sudo docker volume create portainer_data
+sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer \
     --restart=always \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v portainer_data:/data \
